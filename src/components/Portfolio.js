956e1a5a574610e9db7b4003e0f184bbe076c9a9
Shaha007 <Shaham.prince56@gmail.com>
2022-01-23 16:46:43 +0100

    seventh commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import styles from "../components/Portfolio.css";
+import PortfolioImg01 from '../image/IMG-7851.jpg';
+import PortfolioImg06 from '../image/IMG-7856.jpg';
+
+
+
 
 function Portfolio() {
     return (
@@ -7,18 +12,30 @@ function Portfolio() {
         <div className='portfolio-content'>
             <section className='portfolio-section'>
                 <div className='project-container'>
-                        <div className='project-items'> 
+                        <div className='project-items portfolio-img-box' style={{ backgroundImage: `url(${PortfolioImg01})` }} > 
                             <a href=' https://shaham007.github.io/Personal-Website-01/' 
                             className='project-link' target="_blank"> View Project</a> 
                         </div>
-                        <div className='project-items'>
-                            <a href=' https://shaham007.github.io/Personal-Website-01/'
+                        <div className='project-items portfolio-img-box' style={{ backgroundImage: `url(${PortfolioImg01})` }} >
+                            <a href='https://gracious-rosalind-758789.netlify.app'
                             className='project-link' target="_blank"> View Project</a> 
                           </div>
-                        <div className='project-items'> 
+                        <div className='project-items portfolio-img-box' style={{ backgroundImage: `url(${PortfolioImg01})` }} > 
                             <a href=' https://shaham007.github.io/Personal-Website-01/' 
                             className='project-link' target="_blank"> View Project</a>
                          </div>
+                        <div className='project-items portfolio-img-box' style={{ backgroundImage: `url(${PortfolioImg01})` }} >
+                            <a href='https://thirsty-perlman-78585c.netlify.app'
+                                className='project-link' target="_blank"> View Project</a>
+                        </div>
+                        <div className='project-items portfolio-img-box' style={{ backgroundImage: `url(${PortfolioImg01})` }} >
+                            <a href=' https://shaham007.github.io/Personal-Website-01/'
+                                className='project-link' target="_blank"> View Project</a>
+                        </div>
+                        <div className='project-items portfolio-img-box' style={{ backgroundImage: `url(${PortfolioImg01})` }} >
+                            <a href='https://naughty-torvalds-b70f75.netlify.app'
+                                className='project-link' target="_blank"> View Project</a>
+                        </div>
                          
                 </div>
             </section>
